13db0f8e044db717de657eb5e2dec6c8abb29bdf
Music <musicm987@gmail.com>
2021-01-21 21:32:13 -0700

    started with no solution

---
+++ loops.js
@@ -0,0 +1,12 @@
+function vowelsAndConsonants(s) {
+  let vowels = [];
+  let consonants = [];
+  let arrayOfLetters = s.split(',')
+  arrayOfLetters.filter(letter => {
+      if(letter === ){
+         console.log(vowels.push(letter))
+      } else {
+          console.log(consonants.push(letter))
+      }
+  })
+}
