15073d20e9e026bc2b5574ffe64ad6b3dd390a36
Music <musicm987@gmail.com>
2021-01-22 14:54:13 -0700

    all but last test case passed

--- conditionalStatementsSwitch.js
+++ conditionalStatementsSwitch.js
@@ -1,16 +1,61 @@
+// function getLetter(s) {
+//   let letter;
+//   let arrayOfString = s.split("")
+//   arrayOfString.map(eachLetter => {
+//       if(eachLetter[0] === a){
+//           return "A";
+//       } else if (eachLetter[0] === b){
+//           return "B";
+//       } else if (eachLetter[0] === h){
+//           return "C";
+//       } else if(eachLetter[0] === n){
+//           return "D";
+//       }
+//   })
+//   return letter;
+// }
+
+// function getLetter(s) {
+//     let letter;
+//     let arrayOfString = s.split(" ")
+//     arrayOfString.filter(eachLetter => {
+//         if(eachLetter[0] === "a"){
+//             return console.log("A");
+//         } else if (eachLetter[0] === "b"){
+//             return console.log("B");
+//         } else if (eachLetter[0] === "h"){
+//             return console.log("C");
+//         } else if(eachLetter[0] === "n"){
+//             return console.log("D");
+//         }
+//     })
+//     return ""
+// }
+
 function getLetter(s) {
-  let letter;
-  let arrayOfString = s.split("")
-  arrayOfString.map(eachLetter => {
-      if(eachLetter[0] === a){
-          return "A";
-      } else if (eachLetter[0] === b){
-          return "B";
-      } else if (eachLetter[0] === h){
-          return "C";
-      } else if(eachLetter[0] === n){
-          return "D";
-      }
-  })
-  return letter;
+    if (s[0].includes('a')){
+        return console.log('A')
+    } else if (s[0].includes('b')){
+        return console.log('B')
+    } else if (s[0].includes('h')){
+        return console.log('C')
+    } else if (s[0].includes('n')){
+        return console.log('D')
+    }
+    return letter;
 }
+
+getLetter("hey")
+
+// function getLetter(s) {
+
+//     if (s[0].includes('a')){
+//         return "A"
+//     } else if (s[0].includes('b')){
+//         return "B"
+//     } else if (s[0].includes('h')){
+//         return "C"
+//     } else if (s[0].includes('n')){
+//         return "D"
+//     }
+// }
