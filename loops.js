003049845138e915a5aebf616501bdb0bd8dbe79
Music <musicm987@gmail.com>
2021-01-21 21:47:08 -0700

    solved

--- loops.js
+++ loops.js
@@ -1,12 +1,22 @@
+// function vowelsAndConsonants(s) {
+//   let vowels = [];
+//   let consonants = [];
+//   let arrayOfLetters = s.split(',')
+//   arrayOfLetters.filter(letter => {
+//       if(letter === ){
+//         console.log(vowels.push(letter))
+//       } else {
+//         console.log(consonants.push(letter))
+//       }
+//   })
+// }
+
 function vowelsAndConsonants(s) {
-  let vowels = [];
-  let consonants = [];
-  let arrayOfLetters = s.split(',')
-  arrayOfLetters.filter(letter => {
-      if(letter === ){
-         console.log(vowels.push(letter))
-      } else {
-          console.log(consonants.push(letter))
-      }
-  })
+  const vowels = ['a', 'e', 'i', 'o', 'u'];
+  const arrayOfLetters = s.split('')
+  const stringOfVowels = arrayOfLetters.filter(letter => vowels.includes(letter));
+  const stringOfConsonants = arrayOfLetters.filter(letter => !vowels.includes(letter));
+  console.log(stringOfVowels.join('\n') + '\n' + stringOfConsonants.join('\n'));
 }
+
+vowelsAndConsonants("javascriptloops")
