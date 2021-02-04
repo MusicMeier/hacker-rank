63f99f8fc53d54da3748d5abedcab397a77f0ed6
Music <musicm987@gmail.com>
2021-02-03 23:36:59 -0700

    try 1

---
+++ arrowFunctions.js
@@ -0,0 +1,33 @@
+/*
+ * Modify and return the array so that all even elements are doubled and all odd elements are tripled.
+ * 
+ * Parameter(s):
+ * nums: An array of numbers.
+ */
+// function modifyArray(nums) {
+//   let evenArray = [];
+//   let oddArray = [];
+  
+//   nums.filter(number => {
+//       if(number % 2 === 0){
+//           console.log(evenArray = number * 2)
+//       } else {
+//           console.log(oddArray = number * 3)
+//       }
+//   })
+// }
+
+
+function modifyArray(nums) {
+  
+  nums.filter(number => {
+      if(number % 2 === 0){
+          return number * 2
+      } else {
+          return number * 3
+      }
+    })
+    return nums
+}
+
+console.log(modifyArray([1, 2, 3, 4, 5, 6, 7]))
