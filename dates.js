b9a3e6486a98c3b81968ea217153647fa759a805
Music <musicm987@gmail.com>
2021-02-06 19:27:50 -0700

    starting

---
+++ dates.js
@@ -0,0 +1,8 @@
+
+// The days of the week are: "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+function getDayName(dateString) {
+  let dayName;
+  // Write your code here
+  
+  return dayName;
+}
